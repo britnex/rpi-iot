8142982fb7176a58cd33cd9fbf65e543ba637044
britnex <britnex@users.noreply.github.com>
2019-05-04 11:03:22 +0200

    Update firstboot.sh

--- usr/bin/firstboot.sh
+++ usr/bin/firstboot.sh
@@ -5,6 +5,9 @@ if ! cat /boot/cmdline.txt | grep "overlay=yes"; then
   sed -i '1 s/^/overlay=yes /' /boot/cmdline.txt 
 fi
 
+mkinitramfs -o /boot/initramfs.gz
+echo "initramfs initramfs.gz followkernel" >>/boot/config.txt
+
 # disable this service
 systemctl disable firstboot
 systemctl mask firstboot
