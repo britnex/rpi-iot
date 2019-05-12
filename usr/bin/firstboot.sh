35a23edf2c0aa3ec8cc49364cc7ddb11026a3ae5
britnex <britnex@users.noreply.github.com>
2019-05-12 09:20:55 +0200

    Update firstboot.sh

--- usr/bin/firstboot.sh
+++ usr/bin/firstboot.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mkinitramfs -o /boot/initramfs.gz
+set -e
+
+pushd /tmp
+mkinitramfs -o initramfs.gz
+gunzip initramfs.gz
+mkimage -A arm -T ramdisk -C none -n uInitrd -d initramfs /boot/uInitrd
+popd
+
 #enable readonly 
 echo readonly > /boot/readonly
 
