d42e84274755b65617210847714fd0a07fe44131
britnex <britnex@users.noreply.github.com>
2019-05-12 14:30:38 +0200

    Update build-fs.js

--- build-fs.js
+++ build-fs.js
@@ -148,24 +148,27 @@ DEBIAN_FRONTEND=noninteractive apt-get purge -y logrotate
 
 rm -f $(which rpi-update)
 
-chmod -x /etc/cron.daily/man-db || true
-chmod -x /etc/cron.weekly/man-db || true
-
-chmod +x /usr/bin/reboot-rw.sh
-chmod +x /usr/bin/reboot-ro.sh
-
 # enable watchdog
 sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=14/g' /etc/systemd/system.conf
 
 # first boot, enable readonly in firstboot.sh
-chmod +x /usr/bin/firstboot.sh
+chmod 744 /usr/bin/firstboot.sh
 systemctl enable firstboot
 
 DEBIAN_FRONTEND=noninteractive apt-get clean
 
+chmod 444 /etc/cron.daily/man-db || true
+chmod 444 /etc/cron.weekly/man-db || true
+
+chmod 744 /usr/bin/reboot-rw.sh
+chmod 744 /usr/bin/reboot-ro.sh
+
+chmod 644 /etc/ssh/sshd_config
+
 # build uboot compatible initrd
-chmod +x /etc/initramfs-tools/hooks/overlay
-chmod +x /etc/initramfs-tools/scripts/init-bottom/overlay
+chmod 744 /etc/initramfs-tools/hooks/overlay
+chmod 744 /etc/initramfs-tools/scripts/init-bottom/overlay
+
 kernelver=\$(ls -1a /lib/modules | grep -)
 mkinitramfs -o initramfs.gz \${kernelver}
 gunzip initramfs.gz
