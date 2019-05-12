ad811de209691e60de8e1860003c8291c1f8df13
britnex <britnex@users.noreply.github.com>
2019-05-12 08:42:37 +0200

    Update firstboot.sh

--- usr/bin/firstboot.sh
+++ usr/bin/firstboot.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# enable readonly
-#if ! cat /boot/cmdline.txt | grep "overlay=yes"; then
-#  sed -i '1 s/^/overlay=yes /' /boot/cmdline.txt 
-#fi
-
 mkinitramfs -o /boot/initramfs.gz
-#echo "initramfs initramfs.gz followkernel" >>/boot/config.txt
+#enable readonly 
+echo readonly > /boot/readonly
 
 # disable this service
 systemctl disable firstboot
