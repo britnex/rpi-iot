a987ffd6cd2e9a298d43b1211db423ad23bba743
britnex <britnex@users.noreply.github.com>
2019-07-14 13:55:50 +0200

    Update customizeimage.sh

--- src/customizeimage.sh
+++ src/customizeimage.sh
@@ -2,15 +2,11 @@
 set -x
 set -e
 
-DEBIAN_FRONTEND=noninteractive apt-get clean
-
 chmod 444 /etc/cron.daily/man-db || true
 chmod 444 /etc/cron.weekly/man-db || true
 
 chmod 744 /usr/bin/reboot-rw.sh
 chmod 744 /usr/bin/reboot-ro.sh
-
-chmod 744 /etc/init.d/resize2fs_once
 
 chmod 644 /etc/ssh/sshd_config
 
@@ -22,3 +18,11 @@ kernelver=$(ls -1a /lib/modules | grep -)
 mkinitramfs -o initramfs.gz ${kernelver}
 gunzip initramfs.gz
 mkimage -A arm -T ramdisk -C none -n uInitrd -d initramfs /boot/uInitrd
+
+rm -f initramfs.gz
+rm -rf initramfs
+
+# cleanup
+DEBIAN_FRONTEND=noninteractive apt-get purge -y u-boot-tools initramfs-tools
+DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
+DEBIAN_FRONTEND=noninteractive apt-get clean
