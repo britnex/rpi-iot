17e4761023d559ef62d62063b5efe57982f4b055
britnex <britnex@users.noreply.github.com>
2019-07-15 13:48:50 +0200

    Update customizeimage.sh

--- src/customizeimage.sh
+++ src/customizeimage.sh
@@ -2,11 +2,35 @@
 set -x
 set -e
 
+DEBIAN_FRONTEND=noninteractive apt-get install -y u-boot-tools cloud-guest-utils ufw
+
+systemctl enable ssh
+
+# first boot, enable readonly in firstboot.sh
+chmod 744 /usr/bin/firstboot.sh
+systemctl enable firstboot
+
+# disable swap
+dphys-swapfile swapoff
+systemctl disable dphys-swapfile
+DEBIAN_FRONTEND=noninteractive apt-get purge -y dphys-swapfile
+
+DEBIAN_FRONTEND=noninteractive apt-get purge -y logrotate
+
+rm -f $(which rpi-update)
+
+# enable watchdog
+sed -i 's/#RuntimeWatchdogSec=0/RuntimeWatchdogSec=14/g' /etc/systemd/system.conf
+
+DEBIAN_FRONTEND=noninteractive apt-get clean
+
 chmod 444 /etc/cron.daily/man-db || true
 chmod 444 /etc/cron.weekly/man-db || true
 
 chmod 744 /usr/bin/reboot-rw.sh
 chmod 744 /usr/bin/reboot-ro.sh
+
+chmod 744 /etc/init.d/resize2fs_once
 
 chmod 644 /etc/ssh/sshd_config
 
