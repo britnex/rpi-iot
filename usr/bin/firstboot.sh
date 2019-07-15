f7e1c375e5a1aab6a245f6fe61417aedb140e15f
britnex <britnex@users.noreply.github.com>
2019-07-15 13:44:26 +0200

    Update firstboot.sh

--- usr/bin/firstboot.sh
+++ usr/bin/firstboot.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# set hostname to mac address
+if test -e /sys/class/net/eth0/address; then 
+ name="rpi-"$(sed /sys/class/net/eth0/address -e 's/://g')
+ echo "$name" > /etc/hostname
+ hostname $name
+fi
+
 # enable firewall
 ufw default deny
 ufw allow ssh
